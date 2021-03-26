6188a2d4a7e219ff2dc99eefadf77f2e47f95e1b
Abdelrahman Arafat <darkinb255@gmail.com>
2021-03-26 11:49:20 +0200

    secondry button

---
+++ forms/empty-states/Scenes/script.js
@@ -0,0 +1,29 @@
+fetch("../../../HTML/components/sidebar.html")
+.then(response => {
+  return response.text()
+})
+.then(data => {
+  //console.log(data);
+  document.getElementById("side-bar-container").innerHTML = data;
+}).then(()=>{
+    elms = document.getElementById("side-bar-container").getElementsByTagName("li")
+    for (var i = 0; i < elms.length; i++) {
+      console.log(elms[i].id)
+      if(elms[i].id == "scenes")
+      {
+          console.log(elms[i])
+          elms[i].classList.add("page-selected")
+      }
+    }
+});
+
+
+fetch("../../../HTML/components/navbar.html")
+.then(response => {
+  return response.text()
+})
+.then(data => {
+  //console.log(data);
+  document.getElementById("nav-bar-container").innerHTML = data;
+});
+
