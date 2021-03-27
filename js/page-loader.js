9e24419b25a269e53ac834d262795b32339a3eb5
AliMahmoud80 <ali.mahmoud7080@gmail.com>
2021-03-28 01:38:28 +0200

    All Cleaned Up and merged latest team updates

--- js/page-loader.js
+++ js/page-loader.js
@@ -1,3 +1,4 @@
+// Layouts
 fetch("../../../HTML/components/sidebar.html")
 .then(response => {
   return response.text()
@@ -12,7 +13,6 @@ fetch("../../../HTML/components/sidebar.html")
     for (var i = 0; i < elms.length; i++) {
       if(elms[i].id == title.id)
       {
-          console.log(elms[i])
           elms[i].classList.add("page-selected")
       }
     }
@@ -24,7 +24,60 @@ fetch("../../../HTML/components/navbar.html")
   return response.text()
 })
 .then(data => {
-  //console.log(data);
   document.getElementById("nav-bar-container").innerHTML = data;
 });
 
+
+/**
+ * Load a component into specific element|s using it's query selector. 
+ * 
+ * @param {String} component Component name
+ * @param {String} selector Container element|s selector
+ */
+function load(component, selector) {
+  fetch(`../../../HTML/components/${component}.html`)
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    const containers = document.querySelectorAll(selector)
+    if (containers) {
+      containers.forEach(container => {
+        container.innerHTML = data;
+      })
+    }
+  })
+  .catch(err => {
+    console.log(component, err)
+  })
+}
+
+// all component names to be loaded
+const components = [
+  // Page Headers
+  'page-header-plain',
+  'page-header-btn',
+  'page-header-mm-lh-switch',
+  // Modals
+  'walkthrough-modal-1',
+  'walkthrough-modal-2',
+  'walkthrough-modal-3',
+  'walkthrough-modal-4',
+  // Group cards
+  'group-card-automation-box',
+  'group-card-device-sensors',
+  'group-card-equipment',
+  'group-card-gateway',
+  'group-card-scene',
+  'group-card-sensor',
+  'group-card-gateway-empty',
+  'group-card-device-sensors-empty',
+  'group-card-sensors-empty-empty',
+  // Buttons
+  'mm-lh-switch-btn-group'
+]
+
+// Automatically load every component in "components const" into an element with the same component class name.
+components.forEach(async component => {
+  await load(component, `.${component}`)
+})
