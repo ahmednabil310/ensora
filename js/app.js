72ccedbbae696e4d3bd9a5252ff2a60268961e0e
AliMahmoud80 <ali.mahmoud7080@gmail.com>
2021-04-01 08:16:48 +0200

    working on final stage

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$('.humberger').click(function () {
+  $('.side-bar').toggleClass("d-lg-block");
+  $('.side-bar').toggleClass("d-xl-block");
+  $('.main-page').toggleClass("col-xl-10");
+  $('.main-page').toggleClass("col-lg-10");
+  $('.main-navbar').toggleClass("col-lg-10");
+  $('.main-navbar').toggleClass("col-xl-10");
+})
--- js/page-loader.js
+++ js/page-loader.js
@@ -1,5 +1,5 @@
 // Layouts
-fetch("../../../HTML/components/sidebar.html")
+fetch("../components/sidebar.html")
 .then(response => {
   return response.text()
 })
@@ -15,11 +15,11 @@ fetch("../../../HTML/components/sidebar.html")
       {
           elms[i].classList.add("page-selected")
       }
-    }
+    } 
 });
 
 
-fetch("../../../HTML/components/navbar.html")
+fetch("../components/navbar.html")
 .then(response => {
   return response.text()
 })
@@ -28,56 +28,56 @@ fetch("../../../HTML/components/navbar.html")
 });
 
 
-/**
- * Load a component into specific element|s using it's query selector. 
- * 
- * @param {String} component Component name
- * @param {String} selector Container element|s selector
- */
-function load(component, selector) {
-  fetch(`../../../HTML/components/${component}.html`)
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    const containers = document.querySelectorAll(selector)
-    if (containers) {
-      containers.forEach(container => {
-        container.innerHTML = data;
-      })
-    }
-  })
-  .catch(err => {
-    console.log(component, err)
-  })
-}
+// /**
+//  * Load a component into specific element|s using it's query selector. 
+//  * 
+//  * @param {String} component Component name
+//  * @param {String} selector Container element|s selector
+//  */
+// function load(component, selector) {
+//   fetch(`../components/${component}.html`)
+//   .then(response => {
+//     return response.text()
+//   })
+//   .then(data => {
+//     const containers = document.querySelectorAll(selector)
+//     if (containers) {
+//       containers.forEach(container => {
+//         container.innerHTML = data;
+//       })
+//     }
+//   })
+//   .catch(err => {
+//     console.log(component, err)
+//   })
+// }
 
-// all component names to be loaded
-const components = [
-  // Page Headers
-  'page-header-plain',
-  'page-header-btn',
-  'page-header-mm-lh-switch',
-  // Modals
-  'walkthrough-modal-1',
-  'walkthrough-modal-2',
-  'walkthrough-modal-3',
-  'walkthrough-modal-4',
-  // Group cards
-  'group-card-automation-box',
-  'group-card-device-sensors',
-  'group-card-equipment',
-  'group-card-gateway',
-  'group-card-scene',
-  'group-card-sensor',
-  'group-card-gateway-empty',
-  'group-card-device-sensors-empty',
-  'group-card-sensors-empty-empty',
-  // Buttons
-  'mm-lh-switch-btn-group'
-]
+// // all component names to be loaded
+// const components = [
+//   // Page Headers
+//   'page-header-plain',
+//   'page-header-btn',
+//   'page-header-mm-lh-switch',
+//   // Modals
+//   'walkthrough-modal-1',
+//   'walkthrough-modal-2',
+//   'walkthrough-modal-3',
+//   'walkthrough-modal-4',
+//   // Group cards
+//   'group-card-automation-box',
+//   'group-card-device-sensors',
+//   'group-card-equipment',
+//   'group-card-gateway',
+//   'group-card-scene',
+//   'group-card-sensor',
+//   'group-card-gateway-empty',
+//   'group-card-device-sensors-empty',
+//   'group-card-sensors-empty-empty',
+//   // Buttons
+//   'mm-lh-switch-btn-group'
+// ]
 
-// Automatically load every component in "components const" into an element with the same component class name.
-components.forEach(async component => {
-  await load(component, `.${component}`)
-})
+// // Automatically load every component in "components const" into an element with the same component class name.
+// components.forEach(async component => {
+//   await load(component, `.${component}`)
+// })
