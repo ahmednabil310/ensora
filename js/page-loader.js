041ef434863c871e0678a042fec0ebe2f60b5ece
AliMahmoud80 <ali.mahmoud7080@gmail.com>
2021-04-10 06:24:26 +0200

    cleaning up

--- js/page-loader.js
+++ js/page-loader.js
@@ -27,56 +27,56 @@ fetch("../../../HTML/components/navbar.html")
     document.getElementById("nav-bar-container").innerHTML = data;
   });
 
-/**
- * Load a component into specific element|s using it's query selector.
- *
- * @param {String} component Component name
- * @param {String} selector Container element|s selector
- */
-function load(component, selector) {
-  fetch(`../../../HTML/components/${component}.html`)
-    .then((response) => {
-      return response.text();
-    })
-    .then((data) => {
-      const containers = document.querySelectorAll(selector);
-      if (containers) {
-        containers.forEach((container) => {
-          container.innerHTML = data;
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(component, err);
-    });
-}
+// /**
+//  * Load a component into specific element|s using it's query selector.
+//  *
+//  * @param {String} component Component name
+//  * @param {String} selector Container element|s selector
+//  */
+// function load(component, selector) {
+//   fetch(`../../../HTML/components/${component}.html`)
+//     .then((response) => {
+//       return response.text();
+//     })
+//     .then((data) => {
+//       const containers = document.querySelectorAll(selector);
+//       if (containers) {
+//         containers.forEach((container) => {
+//           container.innerHTML = data;
+//         });
+//       }
+//     })
+//     .catch((err) => {
+//       console.log(component, err);
+//     });
+// }
 
-// all component names to be loaded
-const components = [
-  // Page Headers
-  "page-header-plain",
-  "page-header-btn",
-  "page-header-mm-lh-switch",
-  // Modals
-  "walkthrough-modal-1",
-  "walkthrough-modal-2",
-  "walkthrough-modal-3",
-  "walkthrough-modal-4",
-  // Group cards
-  "group-card-automation-box",
-  "group-card-device-sensors",
-  "group-card-equipment",
-  "group-card-gateway",
-  "group-card-scene",
-  "group-card-sensor",
-  "group-card-gateway-empty",
-  "group-card-device-sensors-empty",
-  // "group-card-sensors-empty-empty",
-  // Buttons
-  "mm-lh-switch-btn-group",
-];
+// // all component names to be loaded
+// const components = [
+//   // Page Headers
+//   "page-header-plain",
+//   "page-header-btn",
+//   "page-header-mm-lh-switch",
+//   // Modals
+//   "walkthrough-modal-1",
+//   "walkthrough-modal-2",
+//   "walkthrough-modal-3",
+//   "walkthrough-modal-4",
+//   // Group cards
+//   "group-card-automation-box",
+//   "group-card-device-sensors",
+//   "group-card-equipment",
+//   "group-card-gateway",
+//   "group-card-scene",
+//   "group-card-sensor",
+//   "group-card-gateway-empty",
+//   "group-card-device-sensors-empty",
+//   // "group-card-sensors-empty-empty",
+//   // Buttons
+//   "mm-lh-switch-btn-group",
+// ];
 
-// Automatically load every component in "components const" into an element with the same component class name.
-components.forEach(async (component) => {
-  await load(component, `.${component}`);
-});
+// // Automatically load every component in "components const" into an element with the same component class name.
+// components.forEach(async (component) => {
+//   await load(component, `.${component}`);
+// });
